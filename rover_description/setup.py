a61ee045f9ddc305d8ab2d5b1bb92268b095e8c1
Anushka <anushkav1888@gmail.com>
2024-07-08 17:02:28 +0530

    fix: code cleanup

--- rover_description/setup.py
+++ rover_description/setup.py
@@ -11,9 +11,12 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'urdf'), glob('urdf/*')),
+        (os.path.join('share', package_name, 'urdf', 'bot'), glob('urdf/bot/*')),
+        (os.path.join('share', package_name, 'urdf', 'rover'), glob('urdf/rover/*')),
+        (os.path.join('share', package_name, 'urdf'), glob('urdf/materials.xacro')),
         (os.path.join('share', package_name, 'launch'), glob('launch/*.launch.py')),
-        (os.path.join('share', package_name, 'meshes'), glob('meshes/*'))
+        (os.path.join('share', package_name, 'meshes'), glob('meshes/*')),
+        (os.path.join('share', package_name, 'rviz'), glob('rviz/*'))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
